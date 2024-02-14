b330013fb97c713abf38255be27a55cba8f55ed1
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-02-14 19:27:02 -0300

    feat: patch route on users

--- routes/users.js
+++ routes/users.js
@@ -30,6 +30,30 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.patch("/:id", async (req, res) => {
+  const id = req.params.id;
+  const { name } = req.body;
+  try {
+    const checkClient = await client.query(
+      "SELECT * FROM cliente WHERE id = $1",
+      [id]
+    );
+
+    if (checkClient.rows.length === 0) {
+      return res.status(404).json({ error: "Cliente não encontrado" });
+    }
+
+    const result = await client.query(
+      "UPDATE cliente SET nome = $1 WHERE id = $2 RETURNING *",
+      [name, id]
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("Erro ao atualizar cliente:", error);
+    res.status(500).json({ error: "Erro interno do servidor" });
+  }
+});
+
 process.on("SIGINT", () => {
   client.end();
   console.log("Conexão com o banco de dados encerrada.");
